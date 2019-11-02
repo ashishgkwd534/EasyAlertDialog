7124819407782ee473a7dfe1d410eb504b560503
Ashish Gaikwad <ashishgkwd534@gamil.com>
2019-11-02 23:06:08 +0530

    implemented a alert dialog example to get easy understanding

--- app/src/main/java/com/ags/easyalertdialog/MainActivity.java
+++ app/src/main/java/com/ags/easyalertdialog/MainActivity.java
@@ -2,7 +2,11 @@ package com.ags.easyalertdialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+
+import com.ags.easyalertdialoglibrary.EasyAlertDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showAlert(View view) {
+        EasyAlertDialog.showAlertDialog(this, "Hii", "This is Alert with only positive Button", new EasyAlertDialog.DialogClickListener() {
+            @Override
+            public void onPositiveButtonClick(DialogInterface dialog, int position) {
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onNegativeButtonClick(DialogInterface dialog) {
+
+            }
+        });
+    }
+
+    public void showNegativeAlert(View view) {
+        EasyAlertDialog.showAlertDialog(this, "Hii", "This is Alert with positive and negative Buttons", "Yes! I did it",
+                "close", new EasyAlertDialog.DialogClickListener() {
+                    @Override
+                    public void onPositiveButtonClick(DialogInterface dialog, int position) {
+                        dialog.dismiss();
+                    }
+
+                    @Override
+                    public void onNegativeButtonClick(DialogInterface dialog) {
+                        dialog.dismiss();
+                    }
+                });
     }
 }
